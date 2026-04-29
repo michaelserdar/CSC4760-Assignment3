ab6738d04c1ca5bce0a436157f7f647d869a3c5a
michaelserdar <chefmichaelserdar@gmail.com>
2026-04-29 15:08:53 -0500

    realized I dont need to scatter and linear dist if I am using the copy method

--- problem6/problem6.cpp
+++ problem6/problem6.cpp
@@ -9,22 +9,6 @@ static int partitionSize(int M, int P, int r) {
     return L + (r < R ? 1 : 0);
 }
 
-static void scatterDist(int I, int P, int &p, int &i) {
-    p = I % P; //which process owns the element I
-    i = I / P; //the local index inside the process
-}
-
-static int linearStartIndex(int M, int P, int row) {
-    int start = 0; 
-
-    for (int r = 0; r < row; r++) {
-        start += partitionSize(M, P, r);
-    }
-    return start;
-}
-
-
-
 
 int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
@@ -32,6 +16,59 @@ int main(int argc, char **argv) {
     int nprocs; 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    if (argc != 3) {
+        if (rank == 0) {
+            std::cerr << "Usage: ./problem6 <M> <Q>\n";
+        }
+        MPI_Finalize();
+        return 1;
+    }
+
+    //get the CLI arguments
+    int M = std::atoi(argv[1]);
+    int Q = std::atoi(argv[2]);
+
+    //check for valid inputs
+    if (M <= 0 || Q <= 0 || nprocs % Q != 0) {
+        if (rank == 0) {
+            std::cerr << "Error: M and Q must be positive, and Q must divide nprocs evenly.\n";
+        }
+        MPI_Finalize();
+        return 1;
+    }
+
+     //num. of ranks for each group
+    int P = nprocs / Q; 
+
+    // get the row number for the current rank
+    int row = rank / Q;
+
+    //get the column number for the current rank 
+    int col = rank % Q;
+
+    //row and column communicators for the split
+    MPI_Comm row_rank;
+    MPI_Comm col_rank;
+
+    //split the comm 
+    MPI_Comm_split(MPI_COMM_WORLD, row, rank, &row_rank);
+    MPI_Comm_split(MPI_COMM_WORLD, col, rank, &col_rank);
+
+    //get the partition size 
+    int local_size = partitionSize(M, P, row); //dist across P rows
+
+
+    //create local vectors for each rank
+    std::vector<double> x(local_size);
+    std::vector<double> y(local_size);
+
+    std::vector<double> global_x(M);
+    std::vector<double> global_y(M);
+    std::vector<int> counts(P);
+    std::vector<int> displs(P);
+
+
 
 
 
